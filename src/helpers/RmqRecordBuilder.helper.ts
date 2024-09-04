505c7d1de6197c5bc6d8830448d6c97026997c26
Thomas Kortyka <info@toxictoast.de>
2024-09-04 10:33:54 +0200

    Stringyfy JSON to Base64 String to RmqRecordBuilder

--- src/helpers/RmqRecordBuilder.helper.ts
+++ src/helpers/RmqRecordBuilder.helper.ts
@@ -1,9 +1,11 @@
 import { RmqRecord, RmqRecordBuilder } from '@nestjs/microservices';
 
 export function RmqRecordBuilderHelper<T>(data: T): RmqRecord<T> {
-  return new RmqRecordBuilder(data)
+  const dataString = JSON.stringify(data);
+  const base64String = Buffer.from(dataString).toString('base64');
+  return new RmqRecordBuilder(base64String)
     .setOptions({
-      contentType: 'application/json',
+      contentType: 'application/json; charset=utf-8',
       contentEncoding: 'utf-8',
     })
     .build();
