fad4b2ba9a9cb04d6f221aad3d8fc6d6a6196dc2
Thomas Kortyka <info@toxictoast.de>
2024-10-24 19:42:35 +0200

    fix rmqrecordbuilder

--- src/helpers/RmqRecordBuilder.helper.ts
+++ src/helpers/RmqRecordBuilder.helper.ts
@@ -1,12 +1,11 @@
 import { RmqRecord, RmqRecordBuilder } from '@nestjs/microservices';
 
 export function RmqRecordBuilderHelper<T>(data: T): RmqRecord<T> {
-  const dataString = JSON.stringify(data);
-  const base64String = Buffer.from(dataString).toString('base64');
-  return new RmqRecordBuilder(base64String)
+  return new RmqRecordBuilder()
     .setOptions({
       contentType: 'application/json; charset=utf-8',
       contentEncoding: 'utf-8',
     })
+    .setData(data)
     .build();
 }
